49e8b6b2e1600d0d8b7d01ed770192c9e5a1a964
Josh Moore <jmoore.14@newcastle.ac.uk>
2020-12-07 12:48:38 +0000

    Created User Class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,69 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class User {
-    public static void main(String[] args) {
+    //Attributes
+    private String name;
+    private int dob;
+    private String emailAddress;
+    private int phoneNumber;
+    private int age;
+
+    public User(String name, int dob, String emailAddress, int phoneNumber, int age)
+    {
+        this.name = name;
+        this.dob = dob;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
     }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getDob()
+    {
+        return dob;
+    }
+
+    public String getEmailAddress()
+    {
+        return emailAddress;
+    }
+
+    public int getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public int getAge()
+    {
+        LocalDate today = LocalDate.now();
+        LocalDate dob = LocalDate.of(); //need to research age calculator initialisation
+        Period p = Period.between(birthday, today);
+        return age;
+    }
+
+    public void displayUser()
+    {
+        System.out.println
+                (
+        "Name:  " + this.name +
+                "\n\tDate of Birth:" + this.dob +
+                "\n\tEmail: " + this.emailAddress +
+                "\n\tContact Number: " + this.phoneNumber +
+                "\n\tAge: " + this.age
+                );
+    }
+
+    public void displayUserShortHand()
+    {
+        System.out.println
+                (this.name + "|" + this.phoneNumber);
+    }
+
+
 }
+
