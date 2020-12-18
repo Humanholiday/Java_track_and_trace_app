cfecb67c1d1330f1e70382ec7f3be3e10c5d90f8
vicky.williams <vicky.williams@northumberland.gov.uk>
2020-12-18 15:41:33 +0000

    Created Menu in IO class
    Created the main menu and the sub menu for the IO class.  Will need to populate some of the menu items when Controller class has been completed.

--- src/IO.java
+++ src/IO.java
@@ -1,10 +1,220 @@
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
-
+import java.util.Scanner;
 
 public class IO
 {
-    public IO
-    {}
+    // Attributes
+    boolean exit = false;
+
+
+    // Constructor
+    public IO()
+    {
+
+    }
+
+    // Methods
+
+
+
+    // Header for the menu
+    private void printHeder()
+    {
+        // Creating the header for our menu
+        System.out.println("------------------------------");
+        System.out.println("|     Track and Trace        |");
+        System.out.println("------------------------------");
+    }
+
+    // Header for the sub-menu
+    private void printSubHeder()
+    {
+        // Creating the header for our menu
+        System.out.println("------------------------------");
+        System.out.println("|        Filters Menu         |");
+        System.out.println("------------------------------");
+    }
+
+    // Main menu options
+    private void printMenu()
+    {
+        // Creating options for our menu
+        System.out.println("\nPlease make a selection");
+        System.out.println("1) Record an Event");
+        System.out.println("2) Add Establishment");
+        System.out.println("3) Filters");
+        System.out.println("4) Print Events");
+        System.out.println("5) Print Establishments");
+        System.out.println("6) Exit the program");
+    }
+
+    // Sub menu options
+    private void printSubMenu()
+    {
+        // Creating options for our menu
+        System.out.println("\nPlease make a selection");
+        System.out.println("1) Records by Establishment");
+        System.out.println("2) Records by Date");
+        System.out.println("3) Records by Name");
+        System.out.println("4) Go Back");
+
+    }
+
+    // Method to run the main menu
+    private void runMenu()
+    {
+        // Show the header
+        printHeder();
+
+        // Show the menu unless in exit state
+        while (!exit)
+        {
+            printMenu();
+
+            // Get the menu choice from the user
+            int choice = getInput();
+
+            performAction(choice);
+        }
+    }
+
+    // Method to run the sub menu
+    private void runSubMenu ()
+    {
+        // Show the header
+        printSubHeder();
+
+        // Show the menu unless in exit state
+        while (!exit)
+        {
+            printSubMenu();;
+
+            // Get the menu choice from the user
+            int subChoice = getSubMenuInput();
+
+            performSubMenuAction(subChoice);
+        }
+    }
+
+
+    // Method to handle the input menu choice of the user
+    private int getInput()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        // Set variable to a number that won't conflict with the numbers of our menu options
+        int choice = -1;
+
+        while(choice < 0 || choice > 6)
+        {
+            try
+            {
+                System.out.print("\nPlease enter your choice:");
+
+                // Int isn't a string so have to wrap it in a parse
+                choice = Integer.parseInt(scan.nextLine());
+            }
+            catch(NumberFormatException e)
+            {
+                // If the menu selection number isn't valid then print out an error and ask for another number
+                System.out.println("Invalid Selection.  Please try again");
+            }
+
+        }
+
+        return choice;
+
+    }
+
+    // Method to handle the input of the sub menu choice of the user
+    private int getSubMenuInput()
+    {
+        Scanner scanSub = new Scanner(System.in);
+
+        // Set variable to a number that won't conflict with the numbers of our menu options
+        int subChoice = -1;
+
+        while(subChoice < 0 || subChoice > 4)
+        {
+            try
+            {
+                System.out.print("\nPlease enter your choice:");
+
+                // Int isn't a string so have to wrap it in a parse
+                subChoice = Integer.parseInt(scanSub.nextLine());
+            }
+            catch(NumberFormatException e)
+            {
+                // If the menu selection number isn't valid then print out an error and ask for another number
+                System.out.println("Invalid Selection.  Please try again");
+            }
+
+        }
+
+        return subChoice;
+
+    }
+
+    // Method to do something with the menu choice
+    private void performAction(int choice)
+    {
+        switch (choice)
+        {
+            case 1:
+                // Record an Event
+                break;
+            case 2:
+                // Add an establishment
+                break;
+            case 3:
+                runSubMenu();
+                break;
+            case 4:
+                // Prints Events
+                break;
+            case 5:
+                // Prints Establishments
+                break;
+            case 6:
+                exit = true;
+                System.out.print("Goodbye");
+                break;
+            default:
+                System.out.print("Error has occured");
+
+        }
+    }
+
+    // Method to do something with the sub menu choice
+    private void performSubMenuAction(int subChoice)
+    {
+        switch (subChoice)
+        {
+            case 1:
+                // Records by Establishment
+                break;
+            case 2:
+                // Records by date
+                break;
+            case 3:
+                // Records by Name
+                break;
+            case 4:
+                runMenu();
+                break;
+            default:
+                System.out.print("Error has occured");
+
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        IO menu = new IO();
+        menu.runMenu();
+
+    }
 }
