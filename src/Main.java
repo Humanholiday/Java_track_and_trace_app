38888883a03fb506b4fa227792b5195d59ac67aa
Jack Healy <j.healy12@newcastle.ac.uk>
2021-01-06 16:36:00 +0000

    Update debug method to print error/success messages and display results

--- src/Main.java
+++ src/Main.java
@@ -25,71 +25,347 @@ public class Main {
     public static void debug(IO io)
     {
 
-//        //Hard code 7 user objects
+        System.out.println("\n\nRunning debug method - ");
+
+       //****** HARD CODE 7 USERS *****//
+
+        //CREATE ARRAY OF OBJECTS
         User[] users = new User[7];
-        users[0] = new User
-                ("Jack Healy", LocalDate.of(1985, 6, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
-        users[1] = new User
-                ("Jack Healy", LocalDate.of(1985, 6, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
-        users[2]= new User
-                ("Jack Healy", LocalDate.of(1985, 6, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
-        users[3] = new User
-                ("Jack Healy", LocalDate.of(1985, 6, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
-        users[4]= new User
-                ("Jack Healy", LocalDate.of(1985, 4, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
-        users[5] = new User
-                ("Jack Healy", LocalDate.of(1985, 6, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
-        users[6] = new User
-                ("Jack Healy", LocalDate.of(1985, 6, 8) ,
-                        "jackhealy85@gmail.com", "07515708720");
+        //INITIALISE ID STRING FOR OUTPUT
+        String user = null;
 
-        // hard code 7 establishments
+        /* USER 1 */
+
+        //WRAP IN TRY CATCH
+        try
+        {
+            //UPDATE ID STRING FOR CURRENT OBJECT
+            user = "user1";
+            //ADD OBJECT TO ARRAY
+            users[0] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 8),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            //IF NO EXCEPTIONS, PRINT OUT SUCCESS MESSAGE
+            System.out.println("\n" + user + " " + users[0].getName() + " created");
+        }
+        catch (Exception e)
+            //IF EXCEPTIONS PRINT OUT DETAILS INCLUDING CURRENT OBJECT ID
+        { System.out.println(user + " " + e.getMessage()); }
+
+        /* USER 2 */
+
+        try
+        {
+            user = "user2";
+            users[1] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 8),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            System.out.println(user + " " + users[1].getName() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(user + " " + e.getMessage()); }
+
+        /* USER 3 */
+
+        try
+        {
+            user = "user3";
+            users[2] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 56565),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            System.out.println(user + " " + users[2].getName() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(user + " " + e.getMessage()); }
+
+        /* USER 4 */
+
+        try
+        {
+            user = "user4";
+            users[3] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 3),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            System.out.println(user + " " + users[3].getName() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(user + " " + e.getMessage()); }
+
+        /* USER 5 */
+
+        try
+        {
+            user = "user5";
+            users[4] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 3),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            System.out.println(user + " " + users[4].getName() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(user + " " + e.getMessage()); }
+
+        /* USER 6 */
+
+        try
+        {
+            user = "user6";
+            users[5] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 5),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            System.out.println(user + " " + users[5].getName() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(user + " " + e.getMessage()); }
+
+        /* USER 7 */
+
+        try
+        {
+            user = "user7";
+            users[6] = new User
+                    ("Jack Healy", LocalDate.of(1985, 6, 5),
+                            "jackhealy85@gmail.com", "07515708720");
+
+            System.out.println(user + " " + users[6].getName() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(user + " " + e.getMessage()); }
+
+
+
+        //***** HARD CODE 7 ESTABLISHMENTS *****//
+
+        //CREATE ARRAY OF OBJECTS
         Establishment[] establishments = new Establishment[7];
-        establishments[0] = new Establishment("Some Coffee House", "1 King Street",
-                                                  "FA1 3KE,", 5);
-        establishments[1] = new Establishment("Some Restaurant Place", "1 Queen Street",
+        //INITIALISE ID STRING FOR OUTPUT
+        String establishment = null;
+
+        /* ESTABLISHMENT 1 */
+
+        //WRAP IN TRY CATCH
+        try {
+            //UPDATE ID STRING FOR CURRENT OBJECT
+            establishment = "establishment1";
+            //ADD OBJECT TO ARRAY
+            establishments[0] = new Establishment("Some Coffee House", "1 King Street",
+                    "FA1 3KE,", 5);
+            //IF NO EXCEPTIONS, PRINT OUT SUCCESS MESSAGE
+            System.out.println("\n" + establishment + " " + establishments[0].getName() + " created");
+        }
+        catch (Exception e)
+        //IF EXCEPTIONS PRINT OUT DETAILS INCLUDING CURRENT OBJECT ID
+        { System.out.println("\n" + establishment + " " + e.getMessage()); }
+
+        /* ESTABLISHMENT 2 */
+
+        try {
+            establishment = "establishment2";
+            establishments[1] = new Establishment("Some Restaurant Place", "1 Queen Street",
                                                 "FA2 3KE", 5);
-        establishments[2]= new Establishment("Some Book Place", "1 Regal Street",
+            System.out.println(establishment + " " + establishments[1].getName() + " created");
+        }
+        catch (Exception e)
+         { System.out.println(establishment + " " + e.getMessage()); }
+
+        /* ESTABLISHMENT 3 */
+
+        try {
+            establishment = "establishment3";
+            establishments[2] = new Establishment("Some Book Place", "1 Regal Street",
                                                 "FA4 3KE", 20);
-        establishments[3] = new Establishment("Some Book Place", "1 Regal Street",
-                                                "FA4 3KE", 20);
-        establishments[4] = new Establishment("Some Book Place", "1 Regal Street",
-                                                "FA4 3KE", 20);
-        establishments[5] = new Establishment("Some Book Place", "1 Regal Street",
-                                                "FA4 3KE", 20);
-        establishments[6]= new Establishment("Some Book Place", "1 Regal Street",
-                                                "FA4 3KE", 20);
+            System.out.println(establishment + " " + establishments[2].getName() + " created");
+        }
+        catch (Exception e)
+         { System.out.println(establishment + " " + e.getMessage()); }
+
+        /* ESTABLISHMENT 4 */
+
+        try {
+            establishment = "establishment4";
+            establishments[3] = new Establishment("Some Restaurant Place", "1 Queen Street",
+                                                "FA2 3KE", 5);
+            System.out.println(establishment + " " + establishments[3].getName() + " created");
+        }
+        catch (Exception e)
+         { System.out.println(establishment + " " + e.getMessage()); }
+
+        /* ESTABLISHMENT 5 */
+
+        try {
+            establishment = "establishment5";
+            establishments[4] = new Establishment("Some Restaurant Place", "1 Queen Street",
+                                                "FA2 3KE", 5);
+            System.out.println(establishment + " " + establishments[4].getName() + " created");
+        }
+        catch (Exception e)
+         { System.out.println(establishment + " " + e.getMessage()); }
+
+        /* ESTABLISHMENT 6 */
+
+        try {
+            establishment = "establishment6";
+            establishments[5] = new Establishment("Some Restaurant Place", "1 Queen Street",
+                                                "FA2 3KE", 5);
+            System.out.println(establishment + " " + establishments[5].getName() + " created");
+        }
+        catch (Exception e)
+         { System.out.println(establishment + " " + e.getMessage()); }
+
+        /* ESTABLISHMENT 7 */
+
+        try {
+            establishment = "establishment7";
+            establishments[6] = new Establishment("Some Restaurant Place", "1 Queen Street",
+                                                "FA2 3KE", 5);
+            System.out.println(establishment + " " + establishments[6].getName() + " created" + "\n");
+        }
+        catch (Exception e)
+         { System.out.println(establishment + " " + e.getMessage() + "\n"); }
 
 
-        //hard code 7 events
+
+        //***** HARD CODE 7 EVENTS *****//
+
+        //CREATE ARRAY OF OBJECTS
         Event[] events = new Event[7];
+        //INITIALISE ID STRING FOR OUTPUT
+        String event = null;
+
+        /* EVENT 1 */
+
+        //WRAP IN TRY CATCH
+        try {
+            //UPDATE ID STRING FOR CURRENT OBJECT
+            event = "event1";
+            //ADD OBJECT TO ARRAY
         events[0] = new Event
                 (users[0], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[0]);
+            //IF NO EXCEPTIONS, PRINT OUT SUCCESS MESSAGE
+            System.out.println(event + " " + events[0].getEventID() + " created");
+        }
+        catch (Exception e)
+        //IF EXCEPTIONS PRINT OUT DETAILS INCLUDING CURRENT OBJECT ID
+        { System.out.println(event + " " + e.getMessage()); }
+
+        /* EVENT 2 */
+
+        try {
+            event = "event2";
         events[1] = new Event
                 (users[1], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[1]);
-        events[2]= new Event
+            System.out.println(event + " " + events[1].getEventID() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(event + " " + e.getMessage()); }
+
+        /* EVENT 3 */
+
+        try {
+            event = "event3";
+        events[2] = new Event
                 (users[2], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[2]);
+            System.out.println(event + " " + events[2].getEventID() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(event + " " + e.getMessage()); }
+
+        /* EVENT 4 */
+
+        try {
+            event = "event4";
         events[3] = new Event
                 (users[3], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[3]);
+            System.out.println(event + " " + events[3].getEventID() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(event + " " + e.getMessage()); }
+
+        /* EVENT 5 */
+
+        try {
+            event = "event5";
         events[4] = new Event
                 (users[4], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[4]);
+            System.out.println(event + " " + events[4].getEventID() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(event + " " + e.getMessage()); }
+
+        /* EVENT 6 */
+
+        try {
+            event = "event6";
         events[5] = new Event
                 (users[5], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[5]);
+            System.out.println(event + " " + events[5].getEventID() + " created");
+        }
+        catch (Exception e)
+        { System.out.println(event + " " + e.getMessage()); }
+
+        /* EVENT 7 */
+
+        try {
+            event = "event7";
         events[6] = new Event
                 (users[6], LocalDate.of(2020, 12, 10),
                         LocalTime.of(12, 30), 400, establishments[6]);
+            System.out.println(event + " " + events[6].getEventID() + " created" + "\n");
+        }
+        catch (Exception e)
+        { System.out.println(event + " " + e.getMessage() + "\n"); }
+
+
+
+        //LOOP THROUGH USER ARRAY AND PRINT DISPLAY EACH USER
+        for(int i = 0; i < users.length; i++)
+        {
+            try
+            {
+                System.out.println(users[i].displayUserShortHand() + "\n");
+            }
+            //CATCH ANY EXCEPTIONS AND PRINT MESSAGE INCLUDING INDEX OF ERRONEOUS OBJECT
+            catch (Exception e)
+            { System.out.println("User object at index " + i + " is " + e.getMessage() + "\n"); }
+
+        }
+
+        //LOOP THROUGH USER ARRAY AND PRINT DISPLAY EACH ESTABLISHMENT
+        for(int i = 0; i < establishments.length; i++)
+        {
+            try
+            {
+                System.out.println(establishments[i].displayEstablishment() + "\n");
+            }
+            catch (Exception e)
+            { System.out.println("Establishment object at index " + i + " is " + e.getMessage() + "\n"); }
+
+        }
+
+        //LOOP THROUGH EVENTS ARRAY AND PRINT DISPLAY EACH EVENT
+        for(int i = 0; i < events.length; i++)
+        {
+            try
+            {
+                System.out.println(events[i].displayEvent() + "\n");
+            }
+            catch (Exception e)
+            { System.out.println("Event object at index " + i + " is " + e.getMessage() + "\n"); }
+
+        }
+
+
 //
 //        //CREATE AN ESTABLISHMENT OBJECT - JH
 //
