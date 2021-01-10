c6d5ee882029b884f51055df26bfc171d617515a
vicky.williams <vicky.williams@northumberland.gov.uk>
2021-01-10 15:50:14 +0000

    Refined methods in Controller class for filter by date and filter by name and email address

--- src/Controller.java
+++ src/Controller.java
@@ -259,50 +259,21 @@ public final class Controller
 
     protected void filterEventByUserAndEmail(String name, String emailAdd)
     {
+        try
+        {
+            //Read the csv file that contains the list of events
+            ArrayList<Event> events = getEvents();
 
-        // Read the csv file
-        ArrayList<String> csvText = FileLoader.loadCSVFile("data/events.csv");
-        try {
-            // Read through the csv file until the end
-            for (String line : csvText) {
-                // the last line of the CSV file may be null, so check first!
-                if (line != null) {
-                    // The column split is either a comma or a new line
-                    String[] values = line.split("[,]");
+            for (Event event:events)  // for each event listed in the array
+            {
 
-                    // In order of the Columns in CSV File
-                    String eventID = values[0];
-                    String username = values[1];
-                    LocalDate dob = LocalDate.parse(values[2]);
-                    String email = values[3];
-                    String contactNumber = values[4];
-                    Integer age = Integer.parseInt(values[5]);
-                    LocalDate eventDate = LocalDate.parse(values[6]);
-                    String eventTime = values[7];
-                    Integer partySize = Integer.parseInt(values[8]);
-                    String establishmentName = values[9];
-                    String firstLineAddress = values[10];
-                    String postcode = values[11];
-                    Integer maxOccupancy = Integer.parseInt(values[12]);
+                // if the event in the csv file matches the name and email address variable passed in, then use the displayEvent method in the Event class to output the event
+                if(event.getUser().getName().equalsIgnoreCase(name) && event.getUser().getEmailAddress().equalsIgnoreCase(emailAdd))
+                {
 
-                    // Parse the string event time as  local time
-                    LocalTime localEventTime = LocalTime.parse(eventTime);
-
-                    // If name and email address equals that in the scanner in the IO class then output the record
-                    if (username.equals(name) && email.equals(emailAdd)) {
-                        System.out.println("Event - " + values[1] + ", " + values[6]  + ", " + values[7]  + ", " + values[9]  + ", " + values[8]);
-                    }
-
-                    // Add variables to user constructor
-                    User newUser = new User(username, dob, email, contactNumber);
-
-                    // Add variables to establishment constructor
-                    Establishment newEstablishment = new Establishment(establishmentName, firstLineAddress, postcode, maxOccupancy);
-
-                    // Add variables to event constructor
-                    Event newEvent = new Event(newUser, eventDate, localEventTime, partySize, newEstablishment);
+                    // Display the event
+                    System.out.println(event.displayEvent());
                 }
-
 
             }
         }
@@ -310,55 +281,28 @@ public final class Controller
         {
             System.out.println("Error");
         }
-
     }
 
     protected void filterEventByDate(LocalDate date)
     {
+        try
+        {
+            //Read the csv file that contains the list of events
+            ArrayList<Event> events = getEvents();
 
-        // Read the csv file
-        ArrayList<String> csvText = FileLoader.loadCSVFile("data/events.csv");
-        try {
-            // Read through the csv file until the end
-            for (String line : csvText) {
-                // the last line of the CSV file may be null, so check first!
-                if (line != null) {
-                    // The column split is either a comma or a new line
-                    String[] values = line.split("[,]");
+            for (Event event:events)  // for each event listed in the array
+            {
 
-                    // In order of the Columns in CSV File
-                    String eventID = values[0];
-                    String username = values[1];
-                    LocalDate dob = LocalDate.parse(values[2]);
-                    String email = values[3];
-                    String contactNumber = values[4];
-                    Integer age = Integer.parseInt(values[5]);
-                    LocalDate eventDate = LocalDate.parse(values[6]);
-                    String eventTime = values[7];
-                    Integer partySize = Integer.parseInt(values[8]);
-                    String establishmentName = values[9];
-                    String firstLineAddress = values[10];
-                    String postcode = values[11];
-                    Integer maxOccupancy = Integer.parseInt(values[12]);
+                //For some reason I can't get the date to pass into the if without creating a new variable
+                LocalDate newDate = event.getEventDate();
 
-                    // Parse the string event time as  local time
-                    LocalTime localEventTime = LocalTime.parse(eventTime);
+                // if the event in the csv file matches the name and email address variable passed in, then use the displayEvent method in the Event class to output the event
+                if(newDate.equals(date))
+                {
 
-                    // If Event Date equals that in the scanner in the IO class then output the record
-                    if (eventDate.equals(date)) {
-                        System.out.println("Event - " + values[1] + ", " + values[6]  + ", " + values[7]  + ", " + values[9]  + ", " + values[8]);
-                    }
-
-                    // Add variables to user constructor
-                    User newUser = new User(username, dob, email, contactNumber);
-
-                    // Add variables to establishment constructor
-                    Establishment newEstablishment = new Establishment(establishmentName, firstLineAddress, postcode, maxOccupancy);
-
-                    // Add variables to event constructor
-                    Event newEvent = new Event(newUser, eventDate, localEventTime, partySize, newEstablishment);
+                    // Display the event
+                    System.out.println(event.displayEvent());
                 }
-
 
             }
         }
@@ -366,7 +310,6 @@ public final class Controller
         {
             System.out.println("Error");
         }
-
     }
 
 
